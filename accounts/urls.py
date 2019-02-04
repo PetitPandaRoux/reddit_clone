9d412c0552743ac49514c82c5fbdca5072bb77bc
Sakada Ly <lysakada@gmail.com>
2019-02-04 15:25:31 +0100

    clean app name

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
     url(r'^signup/', views.sign_up, name='signup'),
     url(r'^login/', views.show_login, name='login'),

bda8ac8f32e3408f3772c321d4f36d9521bab2ce
Sakada Ly <lysakada@gmail.com>
2019-02-14 15:03:37 +0100

    added vote functionality

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from . import models
+from .models import Post
 # Create your views here.
 
 def home(request):
-    return render(request,'posts/home.html')
+    posts = Post.objects.order_by('votes_total')
+    return render(request,'posts/home.html', {'posts':posts})
 
 @login_required
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['url']:
-            post = models.Post()
+            post = Post()
             post.title = request.POST['title']
-            post.url = request.POST['url']
+            
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                post.url = request.POST['url']
+            else:
+                post.url = 'http://'+request.POST['url']
+            
             post.pub_date = timezone.datetime.now()
             post.author = request.user
             post.save()
@@ -25,6 +31,17 @@ def create(request):
     else:
         return render(request, 'posts/create.html')
 
+def upvote(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.votes_total += 1 
+    post.save()
+    return redirect('home')
+
+def downvote(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.votes_total -= 1 
+    post.save()
+    return redirect('home')
 
 @login_required
 def edit(request):
